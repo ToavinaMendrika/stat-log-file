392fad03583580735461621183e0b19c715de197
Mendrika Toavina <mendrika.toavina.andriantsivolosy@esti.mg>
2019-03-01 22:44:55 +0300

    function documentation

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import sys
 def file_len(fname):
+    """
+    Nombre de ligne dans le fichier
+
+    Parameters
+    ----------
+    arg1 : file
+        Fichier log à analyser 
+
+    Returns
+    -------
+    int
+        nombre de ligne
+    """
+
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
     return i + 1
+
 # cnts4p16.uwaterloo.ca [30:00:44:50] "GET /icons/ok2-0.gif HTTP/1.0" 200 231
 def count_request(fname, index, filter):
+    """
+    Summary line.
+
+    fonction pour faire analise sur le log par fichier par critère et par filtre
+
+    Parameters
+    ----------
+    arg1 : file
+        Fichier log à analuser au format: cnts4p16.uwaterloo.ca [30:00:44:50] "GET /icons/ok2-0.gif HTTP/1.0" 200 231
+    arg2 : int
+        Index dans la ligne du log apres split() de chaque ligne eg: ligne[1] = [30:00:44:50]
+
+    Returns
+    -------
+    int
+        nombre d'occurence de filter dans index(colone) dans le fichier
+
+    """
     file = open(fname,"r")
     total = file_len(fname)
     num_lines = 0
@@ -33,7 +66,9 @@ def count_request(fname, index, filter):
 
     return get_request
 
+# fonction pour calculer nombre de requête par heure 
 def request_per_hour(fname):
+    """ return: nombre de requête par heur"""
     file = open(fname,"r")
     total = file_len(fname)
     num_lines = 0
@@ -47,18 +82,18 @@ def request_per_hour(fname):
         sys.stdout.flush()
         letter  = line.split(" ")    
         key = letter[1][1:6] 
-        print(key)
-        if key in req:
-            req[key] += 1
-        else:
-            req[key] = 1
+        # print(key)
+        # if key in req:
+        #     req[key] += 1
+        # else:
+        #     req[key] = 1
         
     file.close()
 
     return req
 
-#get_request = count_request("data_project.txt",2,"GET")
+get_request = count_request("data_project.txt",2,"GET")
 #ok_request = count_request("data_project.txt",5,"200")
 #print("\n Resultat: {}".format(ok_request))
-r =  request_per_hour("data_project.txt")
-print(r)
+# r =  request_per_hour("data_project.txt")
+print(get_request)
